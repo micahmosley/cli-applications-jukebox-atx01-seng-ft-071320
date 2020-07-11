631c81d94fce30c1edcc660c5c88ab75c6070949
Micah Mosley <micah.mosley@utexas.edu>
2020-07-11 03:07:52 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -11,7 +11,7 @@ songs = [
 ]
 
 def help 
-  puts "I accept ther following commands:"
+  puts "I accept the following commands:"
   puts "- help : displays this help message"
   puts "- list : displays a list of songs you can play"
   puts "- play : lets you choose a song to play"
